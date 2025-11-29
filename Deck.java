cb66d359bb4f71143e2cb86f2d47d7128eb7dbbf
tongdangdai-311 <dangt12@wit.edu>
2025-11-29 13:31:40 -0500

    Implement createDeck method in Deck class
    Added createDeck method to initialize and shuffle the deck.

--- Deck.java
+++ Deck.java
@@ -1,9 +1,21 @@
 package Application;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
 
-	public Deck() {
-		// TODO Auto-generated constructor stub
+	
+		 public static ArrayList<Card> createDeck() {
+
+		      /*
+		       * Create the size right here
+		       */
+		       
+		        Collections.shuffle(deck);
+
+		        return deck;
+		    
 	}
 
 }
