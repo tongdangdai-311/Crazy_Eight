528cd6624024f8009bd090d91dbc40f18a95b1e5
tongdangdai-311 <dangt12@wit.edu>
2025-11-29 13:30:44 -0500

    Enhance Card class with type, value, and methods
    Added fields for card type and value, and implemented constructor and methods.

--- Card.java
+++ Card.java
@@ -1,9 +1,39 @@
-package Application;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Card {
 
-	public Card() {
-		// TODO Auto-generated constructor stub
-	}
+    private String type;
+    private String value;
+   
 
+   
+    public static final String[] CARD_TYPES = {
+            "heart", "spade", "diamond", "club"
+    };
+
+   
+    public static final String[] CARD_RANKS = {
+            "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"
+    };
+
+  
+    private Card(String type, String value) {
+        this.type = type;
+        this.value = value;
+    
+    }
+
+    public String toString() {
+        return Character.toUpperCase(this.value.charAt(0)) + this.value.substring(1) ;
+    }
+
+   
+   
+
+   
+    public String getValue() { return Character.toUpperCase(this.value.charAt(0)) + this.value.substring(1); }
+
+    
+    public String getType() { return this.type; }
 }
